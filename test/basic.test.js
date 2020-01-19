cfd005f2dbfdd20fe9182464d3ab6ad232acde00
Holger Will <h.will@klimapartner.de>
2020-01-19 13:50:21 +0100

    cleanup

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,22 +1,20 @@
 import '../date-icon.js'
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', async () => {
-      var elem = document.createElement('kaskadi-date-icon')
-      elem.setAttribute('date', '1975-04-07')
-      elem.setAttribute('style', '--outline-color: rgb(255, 0, 0)')
-      document.body.appendChild(elem)
-      const res = await elem.updateComplete
-      elem.should.have.property('date')
-      elem.should.have.property('_date')
-      elem.shadowRoot.querySelector('#monat').textContent.should.equal('Apr 75')
-      elem.shadowRoot.querySelector('#day').textContent.should.equal('7')
-      elem.shadowRoot.querySelector('#name').textContent.should.equal('Montag')
-      var bcr = elem.getBoundingClientRect()
-      bcr.height.should.equal(48)
-      bcr.width.should.equal(48)
-      var cs = getComputedStyle(elem.shadowRoot.querySelector('#outline'))
-      cs.stroke.should.equal('rgb(255, 0, 0)')
-    })
+describe('date-icon', () => {
+  it('testing', async () => {
+    var elem = document.createElement('kaskadi-date-icon')
+    elem.setAttribute('date', '1975-04-07')
+    elem.setAttribute('style', '--outline-color: rgb(255, 0, 0)')
+    document.body.appendChild(elem)
+    const res = await elem.updateComplete
+    elem.should.have.property('date')
+    elem.should.have.property('_date')
+    elem.shadowRoot.querySelector('#monat').textContent.should.equal('Apr 75')
+    elem.shadowRoot.querySelector('#day').textContent.should.equal('7')
+    elem.shadowRoot.querySelector('#name').textContent.should.equal('Montag')
+    var bcr = elem.getBoundingClientRect()
+    bcr.height.should.equal(48)
+    bcr.width.should.equal(48)
+    var cs = getComputedStyle(elem.shadowRoot.querySelector('#outline'))
+    cs.stroke.should.equal('rgb(255, 0, 0)')
   })
 })
