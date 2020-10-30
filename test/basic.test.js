5940abd3b0a89a6f4529555f9e952aa40a6b4415
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-30 13:32:28 +0100

    Extended custom styling tests

--- test/basic.test.js
+++ test/basic.test.js
@@ -11,13 +11,19 @@ describe('kaskadi-date-icon', () => {
     await elem.updateComplete
   })
   it('should handle custom CSS styles', async () => {
-    elem.setAttribute('style', '--outline-color: rgb(255, 0, 0)')
+    const color = 'rgb(255, 0, 0)'
+    elem.setAttribute('style', `--icon-size: 64px; --background-color: ${color}; --outline-color: ${color}; --head-color: ${color}; --day-color: ${color}; --month-color: ${color}; --name-color: ${color}`)
     await elem.updateComplete
     const bcr = elem.getBoundingClientRect()
-    bcr.height.should.equal(48)
-    bcr.width.should.equal(48)
-    const cs = getComputedStyle(elem.shadowRoot.querySelector('#outline'))
-    cs.stroke.should.equal('rgb(255, 0, 0)')
+    bcr.height.should.equal(64)
+    bcr.width.should.equal(64)
+    const getNodeComputedStyle = (selector) => getComputedStyle(elem.shadowRoot.querySelector(selector))
+    getNodeComputedStyle('#bg').fill.should.equal(color)
+    getNodeComputedStyle('#outline').stroke.should.equal(color)
+    getNodeComputedStyle('#head').fill.should.equal(color)
+    getNodeComputedStyle('#day').fill.should.equal(color)
+    getNodeComputedStyle('#month').fill.should.equal(color)
+    getNodeComputedStyle('#name').fill.should.equal(color)
   })
   it('should handle absence of date attribute', async () => {
     elem.should.have.property('date')
